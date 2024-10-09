12ef33242137a36e93b14e91df2cb7109522e41f
Thounny <thounnykeo@gmail.com>
2024-10-09 11:00:35 -0400

    Fahrenheit to Celsius

---
+++ MathScripts/f_to_c.js
@@ -0,0 +1,7 @@
+// Fahrenheit temperature into Celsius
+// (32deg F - 32) * 5 / 9 = C
+
+var degF = 98.6;
+var c = ((degF - 32) * 5) / 9;
+
+console.log(degF + " degrees Fahrenheit is " + c + " in degrees Celsius");
