403ee3c5034cf7cc511138c98ac47aeb65a2a237
Thounny <thounnykeo@gmail.com>
2024-10-09 10:32:23 -0400

    Tip amount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,8 @@
+// tip formula = total bill * tip percentage
+
+var totalBill = 400;
+var tipPer = 0.25;
+
+var tip = totalBill * tipPer;
+
+console.log("The tip on a " + totalBill + " food bill is " + tip);
