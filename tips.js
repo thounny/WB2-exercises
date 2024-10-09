859c12adba473ca0e6c6abc8c427f973b8c827a4
Thounny <thounnykeo@gmail.com>
2024-10-09 10:17:39 -0400

    Tips calculator

---
+++ tips.js
@@ -0,0 +1,15 @@
+// FORMULA Total Due is determined by: Food Cost + Tax + Tip
+
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.0;
+
+// Calculations
+var totalDue = foodCost + tax + tip;
+
+// output
+// console.log(totalDue);
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+console.log("Tip is " + tip.toFixed(2));
+console.log("Total Due is " + totalDue);
