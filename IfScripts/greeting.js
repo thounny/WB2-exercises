42d8c2b37d4dc1649e8c846f9fce8969d0511ee5
Thounny <thounnykeo@gmail.com>
2024-10-10 15:23:21 -0400

    greeting.js

---
+++ IfScripts/greeting.js
@@ -0,0 +1,10 @@
+// Variable that contains the current hour (0-23)
+const hour = 17;
+
+if (hour >= 17) {
+  console.log("Good evening!");
+} else if (hour >= 10) {
+  console.log("Good day!");
+} else {
+  console.log("Good morning!");
+}
