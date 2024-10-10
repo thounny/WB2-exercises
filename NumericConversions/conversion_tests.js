76ca771a5aa084f3af27b748ad02c884e1db19ca
Thounny <thounnykeo@gmail.com>
2024-10-10 13:00:22 -0400

    Finished numeric conversions

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,32 @@
+/* Description: This script tests various numeric conversion techniques
+   Author: Thounny Keo | Year Up United */
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+// parseFloat()
+console.log("parseFloat():");
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+// parseInt()
+console.log("parseInt():");
+console.log(parseInt(a));
+console.log(parseInt(b));
+console.log(parseInt(c));
+console.log(parseInt(d));
+// Number()
+console.log("Number():");
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
+// unary+
+console.log("Unary+:");
+console.log(+a);
+console.log(+b);
+console.log(+c);
+console.log(+d);
